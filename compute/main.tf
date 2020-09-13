2a22f145e40de09ab9a851219b46a4e6b3a3f42a
Ubuntu <ubuntu@ip-172-31-11-139.ec2.internal>
2020-09-13 23:19:47 +0000

    updated compute module

--- compute/main.tf
+++ compute/main.tf
@@ -10,8 +10,30 @@ data "aws_ami" "project-compute-ami" {
   }
 }
 
+data "template_file" "user-init" {
+  count = 2
+  template = "${file("${path.module}/userdata.tpl")}"
+  vars {
+    compute-server-subnets = "${element(var.subnet-ips, count.index)}"
+  }
+}
+
 resource "aws_key_pair" "project-auth" {
   key_name = "${var.public-key-name}"
   public_key = "${file(var.public-key-path)}"
 }
 
+resource "aws_instance" "project-instance" {
+  count = "${var.instance-count}"
+  instance_type = "${var.instance-type}"
+  ami = "${var.aws_ami.project-compute-ami.id}"
+  tags {
+    Name = "${project-name}-${count.index +1}"
+    Environment = "dev"
+  }
+  key_name = "${aws_key_pair.project-auth.id}"
+  vpc_security_group_ids = ["${var.security-group}"]
+  subnet_id = "${element(var.subnets, count.index)}"
+  user_data = "${data.template_file.user-init.*.rendered[count.index]}"
+  
+}
--- compute/vars.tf
+++ compute/vars.tf
@@ -1,4 +1,20 @@
+#compute vars.tf
+variable "project-name" {}
 
 variable "public-key-name" {}
+
 variable "public-key-path" {}
 
+#list of subnets
+variable "subnet-ips" {
+    type = list
+}
+
+#subnet ids
+variable "subnets" {}
+
+variable "instance-count" {}
+
+variable "instance-type" {}
+
+variable "security-group" {}
